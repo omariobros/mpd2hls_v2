7875048714add4242527689cc23c1a385620bf5c
omariobros <omar@omar.is>
2023-09-22 15:01:39 +0000

    Update commands to work in background

--- mpd2hls_v2.sh
+++ mpd2hls_v2.sh
@@ -48,7 +48,7 @@ while [[ $# -gt 0 ]]; do
         KEY="$2"
         shift
         ;;
-        --custom-proxy)
+        -custom-proxy)
         CUSTOM_PROXY="--custom proxy $2"
         shift
         ;;
@@ -57,12 +57,13 @@ while [[ $# -gt 0 ]]; do
         shift
         ;;
         -log-no-prnt)
-        LOGS_NM3U8DL="> logs/N_m3u8DL_RE 2>&1"
-        LOGS_FFMPEG="> logs/ffmpeg 2>&1"
+        LOGS_NM3U8DL="> logs/N_m3u8DL_RE 2>&1 &"
+        LOGS_FFMPEG="> logs/ffmpeg 2>&1 &"
         shift
         ;;
         -no-logs-or-print)
-        NOLOGS="> /dev/null 2>&1"
+        NOLOGS_NM3U8DL="> /dev/null 2>&1 &"
+        NOLOGS_FFMPEG="> /dev/null 2>&1 &"
         shift
         ;;
         *)
@@ -97,11 +98,12 @@ INSTANCE=$(date +%s%N)
 mkdir -p "$FOLDER" "$TEMP_DIR"
 
 # Replace "url" and "keyID:key" with the provided arguments
-COMMAND_1="N_m3u8DL-RE --save-dir $FOLDER --del-after-done true --save-name $FILE --tmp-dir $TEMP_DIR $URL $SHAKA --live-real-time-merge true --thread-count 1 --live-wait-time 2 --live-keep-segments false --mp4-real-time-decryption true --live-pipe-mux true -sv best -sa all --key $KEY --use-system-proxy true $CUSTOM_PROXY -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36' $LOGS_NM3U8DL $NOLOGS"
-COMMAND_2="ffmpeg -re -i \"$FOLDER/$FILE.ts\" -c copy -f hls -sc_threshold 0 -pix_fmt yuv420p -attempt_recovery 1 -recovery_wait_time 1 -hls_flags delete_segments -hls_time 2 -hls_list_size 20 \"/var/www/html/$FOLDER/$FILE.m3u8\" $LOGS_FFMPEG $NOLOGS"
+COMMAND_1="N_m3u8DL-RE --save-dir $FOLDER --del-after-done true --save-name $FILE --tmp-dir $TEMP_DIR $URL $SHAKA --live-real-time-merge true --thread-count 1 --live-wait-time 2 --live-keep-segments false --mp4-real-time-decryption true --live-pipe-mux true -sv best -sa all --key $KEY --use-system-proxy true $CUSTOM_PROXY -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'"
+COMMAND_2="ffmpeg -re -i \"$FOLDER/$FILE.ts\" -c copy -f hls -sc_threshold 0 -pix_fmt yuv420p -attempt_recovery 1 -recovery_wait_time 1 -hls_flags delete_segments -hls_time 2 -hls_list_size 20 \"/var/www/html/$FOLDER/$FILE.m3u8\""
 
 # Execute the commands and save the process IDs to files, we will delay the COMMAND_2 for FFMPEG for 40 sec while N_m3u8DL-RE buffer and creates temp files requried for ffmpeg
-eval "$COMMAND_1 && sleep 40 && $COMMAND_2 &"
+eval "$COMMAND_1 $LOGS_NM3U8DL $NOLOGS_NM3U8DL"
+eval "sleep 30 && $COMMAND_2 $LOGS_FFMPEG $NOLOGS_FFMPEG"
 PID_N_m3u8DL_RE=$!
 PID_ffmpeg=$!
 echo "$PID_N_m3u8DL_RE" > "pid_n_m3u8dl_re_$INSTANCE.txt"
@@ -114,7 +116,9 @@ echo "Your stream should be available at: http://$server_ip/$FOLDER/$FILE.m3u8"
 echo "Hit enter to continue using this terminal"
 
 # Wait for both processes to complete
+wait $PID_N_m3u8DL_RE
 wait $PID_ffmpeg
+
 
 # Clean up the temporary folders/files and process ID files
 cleanup
